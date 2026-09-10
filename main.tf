25a9ffc98f2208128fb271350e1cff24f9e928d7
DAYANAND DEEPAK <30375388+daya1994@users.noreply.github.com>
2026-09-10 19:35:45 +0530

    Initialize Terraform with AWS provider and instance
    Added Terraform configuration for AWS provider and VPC.

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1d"
+}
+
+# Create a VPC
+resource "aws_instance" "web_server" {
+  ami           = "ami-0332d564d76dbd8d6" # Replace with the desired AMI ID
+  instance_type = "t3.micro"
+  tags = {
+    Name = "tomcat-servers"
+  }
+}
