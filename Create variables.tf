b60fe38599fedf577e5a23770c5a1898feef1b3f
DAYANAND DEEPAK <30375388+daya1994@users.noreply.github.com>
2026-09-10 20:31:33 +0530

    Update Create variables.tf

--- Create variables.tf
+++ Create variables.tf
@@ -1,5 +1,5 @@
 variable "ec2_instance_type" {
   description = "The EC2 instance type"
   type        = string
-  default     = "t2.micro"  # Change this to your desired instance type
+  default     = "t3.micro"  # Change this to your desired instance type
 }
